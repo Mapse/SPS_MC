7d2a575fb226668049c3c4aa0e0a9d3ffe8989ba
Mapse Barroso Ferreira Filho <mapse.b@hotmail.com>
2023-07-20 16:04:10 +0200

    Adding path creators - LHE

---
+++ CMSSW_10_6_20_patch1/src/LHE/paths/vai.sh
@@ -0,0 +1,1 @@
+xrdfs xrootd-redir.ultralight.org ls -u /store/group/uerj/mabarros/jpsi_ccbar_3FS_4FS/2017/LHE_minpt6d0 > jpsi_ccbar_3FS_4FS_SPS_2017_13TeV.txt
---
+++ CMSSW_10_6_20_patch1/src/LHE/paths/vai_cernbox.sh
@@ -0,0 +1,1 @@
+xrdfs eosuser.cern.ch ls -u /eos/user/m/mabarros/Monte_Carlo/SPS/jpsi_ccbar_3FS_4FS/lhe_final > jpsi_ccbar_3FS_4FS_SPS_2017_13TeV.txt
