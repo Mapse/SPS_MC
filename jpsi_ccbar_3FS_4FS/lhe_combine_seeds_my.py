ba98a1c6215e3e52f304297d8850bf7d920c13ac
Mapse Barroso Ferreira Filho <mapse.b@hotmail.com>
2023-08-30 05:25:27 +0200

    fixing I/O error

--- jpsi_ccbar_3FS_4FS/lhe_combine_seeds_my.py
+++ jpsi_ccbar_3FS_4FS/lhe_combine_seeds_my.py
@@ -11,56 +11,59 @@ def combine_lhe_seeds(file_in):
     procxs={}
     procnevt={}
     
-    for i, file in enumerate(file_in):         
+    for i, file in enumerate(file_in):   
+        #print(f'Scanning file ({i}): {file}')      
         with open(file, 'r') as stream:  
             #stream=open(file,'r')
             initQ, eventQ, found, iinit, ievent=False, False, False, -1, -1
-            for line in stream:
-                sline=line.replace('\n','')
-                if "<init>" in line or "<init " in line:
-                    initQ=True
-                    iinit=iinit+1
-                elif "</init>" in line or "</init " in line:
-                    initQ=False
-                    iinit=-1
-                elif initQ:
-                    iinit=iinit+1
-                    if iinit == 1:
-                        if i == 0:
-                            firstinit=sline
+            try:
+                for line in stream:
+                    sline=line.replace('\n','')
+                    if "<init>" in line or "<init " in line:
+                        initQ=True
+                        iinit=iinit+1
+                    elif "</init>" in line or "</init " in line:
+                        initQ=False
+                        iinit=-1
+                    elif initQ:
+                        iinit=iinit+1
+                        if iinit == 1:
+                            if i == 0:
+                                firstinit=sline
+                            else:
+                                if not sline == firstinit:
+                                    raise Exception("the beam information of the LHE files is not identical")
+                        elif iinit >= 2:
+                            procid=sline.split()[-1]
+                            xs=float(sline.split()[0])
+                            xserr=float(sline.split()[1])
+                            if procid not in procxs:
+                                procxs[procid]=[(xs,xserr)]
+                                procnevt[procid]=0
+                            else:
+                                procxs[procid].append((xs,xserr))
                         else:
-                            if not sline == firstinit:
-                                raise Exception("the beam information of the LHE files is not identical")
-                    elif iinit >= 2:
-                        procid=sline.split()[-1]
-                        xs=float(sline.split()[0])
-                        xserr=float(sline.split()[1])
-                        if procid not in procxs:
-                            procxs[procid]=[(xs,xserr)]
-                            procnevt[procid]=0
-                        else:
-                            procxs[procid].append((xs,xserr))
-                    else:
-                        raise Exception("should not reach here. Do not understand the <init> block")
-                elif "<event>" in line or "<event " in line:
-                    eventQ=True
-                    ievent=ievent+1
-                elif "</event>" in line or "</event " in line:
-                    eventQ=False
-                    ievent=-1
-                elif eventQ:
-                    ievent=ievent+1
-                    if ievent == 1:
-                        found=False
-                        for procid in procxs.keys():
-                            if ' '+procid+' ' not in sline: continue
-                            procnevt[procid]=procnevt[procid]+1
-                            found=True
-                            break
-                        if not found: raise Exception("do not find the correct proc id !")
-            del stream
-            gc.collect()
-        
+                            raise Exception("should not reach here. Do not understand the <init> block")
+                    elif "<event>" in line or "<event " in line:
+                        eventQ=True
+                        ievent=ievent+1
+                    elif "</event>" in line or "</event " in line:
+                        eventQ=False
+                        ievent=-1
+                    elif eventQ:
+                        ievent=ievent+1
+                        if ievent == 1:
+                            found=False
+                            for procid in procxs.keys():
+                                if ' '+procid+' ' not in sline: continue
+                                procnevt[procid]=procnevt[procid]+1
+                                found=True
+                                break
+                            if not found: raise Exception("do not find the correct proc id !")
+                del stream
+                gc.collect()
+            except:
+                pass
 
     # evaluate the average of cross sections
     procwgts={}
@@ -82,87 +85,89 @@ def combine_lhe_seeds(file_in):
         with open(file, 'r') as stream:
             #stream=open(file,'r')
             headQ, initQ, iinit, ievent, old_wgt, new_wgt, eventQ, rwgtQ, procid, new_evt=True, False, -1, -1, -1, -1, False, False, None, ''
-            for line in stream:
-                sline=line.replace('\n','')
-                if "<init>" in line or "<init " in line:
-                    initQ,headQ=True, False
-                    iinit=iinit+1
-                    if i==0: inits.append(sline)
-                elif headQ and i == 0:
-                    headers.append(sline)
-                elif "</init>" in line or "</init " in line:
-                    initQ,iinit=False,-1
-                    if i==0: inits.append(sline)
-                elif initQ:
-                    iinit=iinit+1
-                    if iinit == 1:
-                        if i == 0:
-                            firstinit=sline
-                            inits.append(sline)
+            try:
+                for line in stream:
+                    sline=line.replace('\n','')
+                    if "<init>" in line or "<init " in line:
+                        initQ,headQ=True, False
+                        iinit=iinit+1
+                        if i==0: inits.append(sline)
+                    elif headQ and i == 0:
+                        headers.append(sline)
+                    elif "</init>" in line or "</init " in line:
+                        initQ,iinit=False,-1
+                        if i==0: inits.append(sline)
+                    elif initQ:
+                        iinit=iinit+1
+                        if iinit == 1:
+                            if i == 0:
+                                firstinit=sline
+                                inits.append(sline)
+                            else:
+                                if not sline == firstinit:
+                                    raise Exception("the beam information of the LHE files is not identical")
+                        elif iinit >= 2:
+                            if i == 0:
+                                procid=sline.split()[-1]
+                                xs,err=procxs[procid]
+                                sline=sline.split()
+                                sline[0]="%16.10E"%xs
+                                sline[1]="%16.10E"%err
+                                sline="    "+"    ".join(sline)
+                                inits.append(sline)
                         else:
-                            if not sline == firstinit:
-                                raise Exception("the beam information of the LHE files is not identical")
-                    elif iinit >= 2:
-                        if i == 0:
-                            procid=sline.split()[-1]
-                            xs,err=procxs[procid]
-                            sline=sline.split()
-                            sline[0]="%16.10E"%xs
-                            sline[1]="%16.10E"%err
-                            sline="    "+"    ".join(sline)
-                            inits.append(sline)
-                    else:
-                        raise Exception("should not reach here. Do not understand the <init> block")
-                elif "<event>" in line or "<event " in line:
-                    eventQ=True
-                    ievent=ievent+1
-                    #events.append(sline)
-                    new_evt = new_evt  + sline
-                    new_evt = new_evt + '\n'
-                elif "</event>" in line or "</event " in line:
-                    eventQ, ievent, old_wgt, new_wgt=False, -1, -1, -1
-                    #events.append(sline)
-                    new_evt = new_evt + sline
-                    new_evt = new_evt + '\n'
-                elif eventQ:
-                    ievent=ievent+1
-                    if ievent == 1:
-                        found=False
-                        for procid in procxs.keys():
-                            if ' '+procid+' ' not in sline: continue
-                            try:
-                                old_wgt=float(sline.split()[2])
-                            except:
-                                print(f'File corrupted: {file}')
-                                print('Skipping...')
-                                continue
-                            new_wgt=procwgts[procid]
-                            procpos=sline.index(' %12.6E '%old_wgt)
-                            found=True
-                            sline=sline[:procpos]+(' %12.6E'%(new_wgt))+sline[procpos+len(' %12.6E'%old_wgt):]
-                            break
-                        if not found: raise Exception("do not find the correct proc id !")
-                        #procpos=sline.index(' '+procid)
-                        #sline=sline[:procpos]+(' %d'%(offset+1+i))+sline[procpos+len(' '+procid):]
-                    elif '<rwgt>' in line or '<rwgt ' in line:
-                        rwgtQ=True
-                    elif "</rwgt>" in line or "</rwgt " in line:
-                        rwgtQ=False
-                    elif rwgtQ:
-                        procpos1=sline.index("'>")
-                        procpos2=sline.index("</")
-                        wgt=sline[(procpos1+2):procpos2]
-                        wgt=float(wgt)
-                        sline=sline[:(procpos1+2)]+(" %12.6E "%(wgt*new_wgt/old_wgt))+sline[procpos2:]
+                            raise Exception("should not reach here. Do not understand the <init> block")
+                    elif "<event>" in line or "<event " in line:
+                        eventQ=True
+                        ievent=ievent+1
+                        #events.append(sline)
+                        new_evt = new_evt  + sline
+                        new_evt = new_evt + '\n'
+                    elif "</event>" in line or "</event " in line:
+                        eventQ, ievent, old_wgt, new_wgt=False, -1, -1, -1
+                        #events.append(sline)
+                        new_evt = new_evt + sline
+                        new_evt = new_evt + '\n'
+                    elif eventQ:
+                        ievent=ievent+1
+                        if ievent == 1:
+                            found=False
+                            for procid in procxs.keys():
+                                if ' '+procid+' ' not in sline: continue
+                                try:
+                                    old_wgt=float(sline.split()[2])
+                                except:
+                                    print(f'File corrupted: {file}')
+                                    print('Skipping...')
+                                    continue
+                                new_wgt=procwgts[procid]
+                                procpos=sline.index(' %12.6E '%old_wgt)
+                                found=True
+                                sline=sline[:procpos]+(' %12.6E'%(new_wgt))+sline[procpos+len(' %12.6E'%old_wgt):]
+                                break
+                            if not found: raise Exception("do not find the correct proc id !")
+                            #procpos=sline.index(' '+procid)
+                            #sline=sline[:procpos]+(' %d'%(offset+1+i))+sline[procpos+len(' '+procid):]
+                        elif '<rwgt>' in line or '<rwgt ' in line:
+                            rwgtQ=True
+                        elif "</rwgt>" in line or "</rwgt " in line:
+                            rwgtQ=False
+                        elif rwgtQ:
+                            procpos1=sline.index("'>")
+                            procpos2=sline.index("</")
+                            wgt=sline[(procpos1+2):procpos2]
+                            wgt=float(wgt)
+                            sline=sline[:(procpos1+2)]+(" %12.6E "%(wgt*new_wgt/old_wgt))+sline[procpos2:]
 
-                    #events.append(sline)
-                    new_evt = new_evt  + sline
-                    new_evt = new_evt + '\n'
-            
+                        #events.append(sline)
+                        new_evt = new_evt  + sline
+                        new_evt = new_evt + '\n'
+            except:
+                pass
             """ if i < 97:
                 pass
             elif (i > 96) & (i < 140): """
-            print(f'File: {i+1}')
+            #print(f'Processing file: {i+1}')
             text_opt = '\n'.join(headers)+'\n'
             text_opt = text_opt +'\n'.join(inits)+'\n'
             text_opt = " ".join([text_opt, new_evt])
@@ -176,7 +181,7 @@ def combine_lhe_seeds(file_in):
                 stream.write(text_opt)
                 #del stream
                 #gc.collect()
-            print(f'Memory used: {psutil.Process().memory_info().rss / (1024 * 1024)}') # Converts bytes in MB
+            #print(f'Memory used: {psutil.Process().memory_info().rss / (1024 * 1024)}') # Converts bytes in MB
             
             #stream=open(final_file,'w')
             #text_opt = text_opt + new_evt 
@@ -204,7 +209,7 @@ if __name__ == '__main__':
     from numpy import array, append
 
     # Model for the paths
-    job_paths = '/eos/user/m/mabarros/Monte_Carlo/SPS/jpsi_ccbar_3FS_4FS/condor_jobs_organized/'    
+    job_paths = '/eos/user/m/mabarros/Monte_Carlo/SPS/SPS_MC/jpsi_ccbar_3FS_4FS/condor_jobs_organized/'    
 
     files_in = []
     array_in = array([])
@@ -215,14 +220,14 @@ if __name__ == '__main__':
                 array_in = append(array_in, file.path)
 
     # Loop over the file list
-    print(f'Number of files to process: {len(array_in[:])}')
+    #print(f'Number of files to process: {len(array_in[:])}')
 
     tstart = time.time()
 
-    print(f'Calculating average cross section and doing reweighting..')
+    #print(f'Calculating average cross section and doing reweighting.. Grab a coffee :)')
     #print(array_in[3000:3010])
     combine_lhe_seeds(array_in[:]) 
-    print(f'Finished in: {time.time() - tstart}')
+    #print(f'Finished in: {time.time() - tstart}')
 
 ############################# OLD #############
 """ if __name__ == '__main__':
